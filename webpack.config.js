6b79b97411cd6ef81b310cf710a210a8c265a6fa
medev21 <benmarteen07@gmail.com>
2018-11-10 18:37:36 -0500

    set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const config = {
+    entry: __dirname + '/client/js/index.jsx',
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+        {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['@babel/preset-env', '@babel/react']
+            }
+        },
+        {
+            test: /\.(png|jpg|svg|gif|mp4|mov)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '/assets/[name]-[hash:8].[ext]'
+                }
+            }]
+        },
+        {
+            test: /\.scss$/,
+            loader: 'style-loader!css-loader!sass-loader'
+        }
+        ]
+    },
+    devServer: {
+        publicPath: '/',
+        contentBase: __dirname + '/build',
+        port: 5000,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './index.html', to: './index.html' }
+        ])
+    ]
+}
+
+
+module.exports = config
