c8a08ff2476a4668c27ce4a5eded92f770518515
medev21 <benmarteen07@gmail.com>
2018-11-11 15:27:00 -0500

    build notes schema

---
+++ models/notesSchema.js
@@ -0,0 +1,10 @@
+//notesSchema
+const mongoose = require('mongoose');
+const Schema = mongoose.schema;
+
+const noteSchema = new Schema({
+    description: string,
+    title: string,
+    updated: { type: Date, default: Date.now },
+    pinned: Boolean
+});
