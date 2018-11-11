3b511b252ce41a69c070e0a17b1cb97e0eef5ce2
medev21 <benmarteen07@gmail.com>
2018-11-11 15:52:51 -0500

    install body parser dependency and  configure it server.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,7 @@
 //routes.js
 const express = require('express');
 const router = express.Router();
+const Notes = require('../../models/notesSchema');
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '../../build/index.html'));
--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const path = require('path');
 const router = require('./routes/routes.js')
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const DBUSER = process.env.DBUSER;
 const DBPASSWORD = process.env.DBPASSWORD;
 
 //tells express frontend will reside in client folder
 app.use(express.static(path.join(__dirname, '../build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //connect to mongoDB
 mongoose.connect(`mongodb://${DBUSER}:${DBPASSWORD}@ds141812.mlab.com:41812/note_app_db`);
