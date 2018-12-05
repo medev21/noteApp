934775c381cf9ac7171f9d3b630bee18a6ba8810
medev21 <benmarteen07@gmail.com>
2018-12-05 12:52:52 -0500

    move insertnewnote function to app.js, add babel class properties

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import '../css/App.scss';
+import Axios from 'axios';
+import Add from './components/Add';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      description: '',
+      pinned: null,
+    }
+  };
+
+  insertNewNote = (e) => {
+    Axios.post('/insert', {
+      title: '',
+      description: '',
+      pinned: false
+    }).then(function (response){
+      console.log(response);
+    }).catch(function(error){
+      console.log('Add.jsx-',error)
+    });
+  };
+
   render() {
     return (
       <div className="App">
--- client/js/components/Add.jsx
+++ client/js/components/Add.jsx
@@ -1,29 +1,8 @@
 //client/component/Add.jsx
 import React from 'react';
-import Axios from 'axios';
 import {Link} from 'react-router-dom';
 
 class Add extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            title: '',
-            description: '',
-            pinned: null,
-        }
-    }
-
-    insertNewNote = (e) => {
-        Axios.post('/insert', {
-            title: '',
-            description: '',
-            pinned: false
-        }).then(function (response){
-            console.log(response);
-        }).catch(function(error){
-            console.log('Add.jsx-',error)
-        });
-    };
 
     render() {
         return(
