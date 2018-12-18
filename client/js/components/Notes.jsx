56ab2db947059a99d411fc36a408628a5fb6d933
medev21 <benmarteen07@gmail.com>
2018-12-17 22:36:07 -0500

    add note button static

--- client/js/components/Notes.jsx
+++ client/js/components/Notes.jsx
@@ -1,26 +1,33 @@
 //client/component/Notes.jsx
 import React from 'react';
 import NoteItem from './NoteItem';
+import '../../css/Notes.scss';
 
 class Notes extends React.Component {
 
     render() {
         let notes = this.props.notes;
         return (
-            <div>
-                <ul>
-                    {notes.map((note) => {
-                        return (
-                            <li key={note._id}>
-                                <NoteItem 
-                                    note={note} 
-                                    onUpdate={this.props.onUpdate} 
-                                    onDelete={this.props.onDelete}
-                                />
-                            </li>
-                        )
-                    })}
-                </ul>
+            <div className="noteListSection">
+                <div>
+                    <ul>
+                        {notes.map((note) => {
+                            return (
+                                <li key={note._id}>
+                                    <NoteItem 
+                                        note={note} 
+                                        onUpdate={this.props.onUpdate} 
+                                        onDelete={this.props.onDelete}
+                                    />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+
+                <div className="addNoteSection">
+                    +
+                </div>
             </div>
             
         );
