adc812ce6e73d72bdb4fa75a600b34cba069c9c4
medev21 <benmarteen07@gmail.com>
2018-12-27 16:08:28 -0500

    import modal css in CreateModal component

--- client/js/components/CreateModal.jsx
+++ client/js/components/CreateModal.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../../css/Modal.scss'
 
 class CreateModal extends React.Component {
 
     render(){
         return(
-            <p>this is the create Modal</p>
+            <div className={showHideClassName}>
+                <div className="modal-main">
+                    <p>this is the create Modal</p>
+                </div>
+            </div>
         );
     }
 
