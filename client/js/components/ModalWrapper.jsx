1ddd60846cf49228cb5d2c06b0fb2949cc6e35ac
medev21 <benmarteen07@gmail.com>
2018-12-28 14:00:00 -0500

    close modal after clicking outside of main modal

--- client/js/components/ModalWrapper.jsx
+++ client/js/components/ModalWrapper.jsx
@@ -5,12 +5,31 @@ class ModalWrapper extends React.Component{
 
     handleCloseModal = () => {
         this.props.close();
+    };
+
+    componentWillMount() {
+        document.addEventListener('click', this.handleClick, false);
+    };
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClick, false);
+    };
+
+    handleClick = (e) => {
+        if(this.node.contains(e.target)){
+            console.log('this is the node')
+            //the click is inside the main modal
+            return;
+        }
+
+        //exit the modal if clicking outside the modal
+        this.handleCloseModal();
     }
 
     render(){
         return(
             <div className="modal display-block">
-                <div className="modal-main">
+                <div className="modal-main" ref={node => {this.node = node;}}>
                     {this.props.children}
                 </div>
 
