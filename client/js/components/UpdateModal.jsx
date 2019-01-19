a9b31b851d0b0402521628680f9094fe87d391f8
medev21 <benmarteen07@gmail.com>
2019-01-19 14:11:28 -0500

    pass note data from NoteItem to UpdateModal component

--- client/js/components/UpdateModal.jsx
+++ client/js/components/UpdateModal.jsx
@@ -3,10 +3,41 @@ import ModalWrapper from './ModalWrapper';
 
 class UpdateModal extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.note._id,
+            title: this.props.note.title,
+            description: this.props.note.description,
+            pinned: this.props.note.pinned,
+            updated: this.props.note.updated
+        }
+    };
+
+    handleTitle = (event) => {
+        this.setState({
+            title: event.target.value
+        });
+    };
+
+    handleDescription = (event) => {
+        this.setState({
+            description: event.target.value
+        });
+    };
+
+    handleUpdateNote = () => {
+        let noteId = this.state.id;
+        let updatedTitle = this.state.title;
+        let updatedDescription = this.state.description;
+        let updatedPinned = this.state.pinned;
+        this.props.onUpdate(noteId, updatedTitle, updatedDescription, updatedPinned);
+    };
+
     render(){
         return(
             <ModalWrapper close={this.props.close}>
-                <h1>Hello world</h1>
+                <p>{this.props.note.title}</p>
             </ModalWrapper>
         );
     }
