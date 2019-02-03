c777b2b9aed25c2819337b3384166c40c78c556c
medev21 <benmarteen07@gmail.com>
2019-02-02 22:04:44 -0500

    add tooltip message based on pin boolean status

--- client/js/components/Tooltip.jsx
+++ client/js/components/Tooltip.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 class ToolTip extends React.Component {
 
     render(){
+
+        const tooltipMsg = this.props.pinBool ? "Unpin note" : "Pin note";
+
         return(
             <span className="tooltip">
-                <div className="tooltipBubble tooltipPosition">Unpin note</div>
+                <div className="tooltipBubble tooltipPosition">{tooltipMsg}</div>
             </span>
         )
     }
