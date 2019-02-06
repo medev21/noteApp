15e4ad1e1e1ff53e84d4a0caf74392ec25830cc5
medev21 <benmarteen07@gmail.com>
2019-02-05 19:38:56 -0500

    add User model

---
+++ models/usersScherma.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    note: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Notes',
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {type: String, required: true}
+});
+
+module.exports = mongoose.model('User', userSchema)
