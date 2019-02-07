6b96ad20cf00b778f5721cd26e1107bdef3a9ef0
medev21 <benmarteen07@gmail.com>
2019-02-06 20:06:30 -0500

    add basic login route for users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -35,6 +35,21 @@ router.post('/signup', (req, res, next) => {
     });    
 });
 
+router.post('/login', (req,res,next) => {
+    User.findOne({email: req.body.email})
+    .exec()
+    .then(user => {
+        res.status(200).json({
+            message: 'Auth Success'
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    })
+});
+
 router.delete('/:userId', (req,res,next) => {
     User.deleteOne({_id: req.params.userId})
     .exec()
