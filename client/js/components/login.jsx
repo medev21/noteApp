b32689f82fff6135dbb3e839a5cba8771d050990
medev21 <benmarteen07@gmail.com>
2019-02-10 17:20:23 -0500

    set up login page

---
+++ client/js/components/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Redirect from 'react-router-dom';
+
+class Login extends React.Component {
+
+
+    render(){
+        return(
+            <h1>this is the login page</h1>
+        )
+    }
+
+}
+
+export default Login;
