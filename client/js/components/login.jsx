371a848780665d2403cf63d8e0e678b653fe33a1
medev21 <benmarteen07@gmail.com>
2019-02-10 22:11:34 -0500

    set up login form

--- client/js/components/login.jsx
+++ client/js/components/login.jsx
@@ -3,10 +3,33 @@ import Redirect from 'react-router-dom';
 
 class Login extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state ={
+            email: '',
+            password: ''
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    };
 
     render(){
         return(
-            <h1>this is the login page</h1>
+            <form onSubmit={this.handleSubmit}>
+                <div className="emailSection">
+                    <input type="text"  name="email" placeholder="email" onChange={this.handleChange}/>
+                </div>
+                <div className="passwordSection">
+                    <input type="password" name="password" placeholder="password" onChange={this.handleChange}/>
+                </div>
+                <div className="submitSection">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         )
     }
 
