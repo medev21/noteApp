1b195de2988808a3b6a45b327c3ef05352f20a9d
medev21 <benmarteen07@gmail.com>
2019-02-12 10:33:31 -0500

    verify token in get notes route

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -2,13 +2,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
 
-    console.log(req.headers.authorization);
-    // try{
-    //     const token = 
-
-    // }catch(error){
-    //     return res.status(401).json({
-    //         message: 'Auth Failed'
-    //     })
-    // }
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        next();
+    }catch(error){
+        return res.status(401).json({
+            message: 'Auth Failed'
+        })
+    }
 };
