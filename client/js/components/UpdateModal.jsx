39c7e81e6f41090a76a438cd86bf83aa54543a1d
medev21 <benmarteen07@gmail.com>
2019-02-16 17:41:49 -0500

    add tooltip to pin in update modal

--- client/js/components/UpdateModal.jsx
+++ client/js/components/UpdateModal.jsx
@@ -3,6 +3,7 @@ import ModalWrapper from './ModalWrapper';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbtack, faPlus } from '@fortawesome/free-solid-svg-icons'
+import Tooltip from './Tooltip';
 
 library.add(faThumbtack, faPlus)
 
@@ -15,7 +16,8 @@ class UpdateModal extends React.Component{
             title: this.props.note.title,
             description: this.props.note.description,
             pinned: this.props.note.pinned,
-            updated: this.props.note.updated
+            updated: this.props.note.updated,
+            tooltipBool: false
         }
     };
 
@@ -47,15 +49,37 @@ class UpdateModal extends React.Component{
         this.props.update(event,noteId, updatedTitle, updatedDescription, updatedPinned);
     };
 
+    handleShowTooltip = () => {
+        this.setState({tooltipBool: true});
+    };
+
+    handleCloseTooltip = () => {
+        this.setState({tooltipBool: false});
+    };
+
     render(){
         const isPinned = this.state.pinned;
+        const tooltipStatus = this.state.tooltipBool
+        let tooltip;
+
+        if(tooltipStatus){
+            tooltip = <Tooltip pinBool={isPinned}/>
+        }
 
         return(
             <ModalWrapper close={this.props.close}>
                 {/* <p>{this.props.note.title}</p> */}
                 <form onSubmit={this.handleUpdateNote}>
                     <div className="pinSection">
-                        <button onClick={this.handlePinnedUpdate}><FontAwesomeIcon transform={isPinned ? "rotate-0" : "rotate-45"} icon="thumbtack" size="1x"/></button>
+                        {tooltip}
+                        <FontAwesomeIcon 
+                            transform={isPinned ? "rotate-0" : "rotate-45"} 
+                            icon="thumbtack" 
+                            size="1x" 
+                            onClick={this.handlePinnedUpdate} 
+                            onMouseOver={this.handleShowTooltip} 
+                            onMouseOut={this.handleCloseTooltip}
+                        />
                     </div>
                     <div className="titleSection">
                         <input type="text" value={this.state.title} onChange={this.handleTitleUpdate}/>
@@ -64,7 +88,7 @@ class UpdateModal extends React.Component{
                         <textarea type="text" value={this.state.description} onChange={this.handleDescriptionUpdate}/>
                     </div>
                     <div className="submitSection">
-                        <button type="submit"><FontAwesomeIcon icon="plus" size="1x"/></button>
+                        <button type="submit">Update</button>
                     </div>
                 </form>
             </ModalWrapper>
