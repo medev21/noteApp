3c7aa1823804a7f6bdf841e9370a916a174f9065
medev21 <benmarteen07@gmail.com>
2019-02-18 12:21:41 -0500

    add jsx ext in login

--- client/js/components/login.jsx
+++ client/js/components/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 import Apis from '../utils/Apis';
-import '../../css/login.scss';
+import '../../css/Login.scss';
 
 class Login extends React.Component {
 
