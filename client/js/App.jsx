6cafec53d8582285aa69cc28d080d8be43a6f96e
medev21 <benmarteen07@gmail.com>
2019-02-18 11:29:14 -0500

    remove unneccessary imports and comments

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import '../css/App.scss';
-import Apis from './utils/Apis';
-import Notes from './components/Notes';
-import Navbar from './components/Navbar';
 import Routes from './components/Routes';
 
 class App extends Component {
